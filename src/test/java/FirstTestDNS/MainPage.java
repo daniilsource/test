7ed6c0fabebe2003879965ec1f08a3632cd8f150
daniilsource <daniilsource@gmail.com>
2021-08-04 20:03:23 +0300

    Test

--- src/test/java/FirstTestDNS/MainPage.java
+++ src/test/java/FirstTestDNS/MainPage.java
@@ -1,8 +1,27 @@
 package FirstTestDNS;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class MainPage {
 
-    ;
+    public WebDriver driver;
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver = driver;
+
+
+    }
+    @FindBy(xpath = "//*[@id=\"homepage-desktop-menu-wrap\"]/div/div[10]/div[1]/a")
+    WebElement autoCategory;
+
+
+
+    public void goNext() {
+        autoCategory.click();
+    }
+
 }
